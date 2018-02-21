82cd670248269f4f7e1df514f9c35c55b5385c3d
Ttech <Ttech@users.noreply.github.com>
2018-02-20 16:36:02 -0800

    Upload existing version of easymasq before cleanup
    A basic DNS blocking tool that takes EasyList (AdBlock Plus/UBlock) or DNS Host files and performs syncing on the lists to build a blacklist of domains. This means you can set up cron or other tools to automatically pull down the latest adblocks without the need of any additional software other then wget/curl, sed, tr, and a shell. This is ideal for a router or other embedded systems which might not have the resources to use larger scripts or web interfaces.

---
+++ easymasq.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+# a simple, easy to use shell downloader
+# you will need wget or curl, sed, and tr to make this run
+# supports easylist files and adblock/dns blacklisting files (host files etc)
+
+# feel free to change these temp files
+TMP="/tmp/easymasqlist.tmp" # total temp for final processing
+LTMP="/tmp/easymasq.genlist" # current list temp
+
+#TODO: update this to make it a bit more powerful when checking for pull request
+pull(){
+	which curl > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		curl -s "$1"
+	else
+		wget -q $1 -O -
+	fi
+}
+
+if [ $# -ne 2 ]; then
+	echo "invalid arguments"
+	echo "command [SOURCE LIST] [DESTINATION]"
+	exit 1
+fi
+
+while IFS= read -r list
+do
+	if echo "$list" | grep -qv "^#\|^$"; then
+		pull $list > $LTMP
+		if [ -f $LTMP ]; then
+			if grep -q "||.*^" $LTMP; then
+				# its easy list
+				echo "processing easylist file from $list as $LTMP"
+				grep -v "^#" $LTMP | sed -n "s/||\(.*\)\^.*/\1/p">>$TMP
+			else
+				# its not an easy list
+				echo "processing dns list file from $list as $LTMP"
+				grep -v "^#" $LTMP | sed -e "s/127.0.0.1//g" -e "s/0.0.0.0//g" | tr -d "[:blank:]">>$TMP
+			fi
+			rm "$LTMP"
+		fi
+	fi
+done <"$1"
+
+# and complete
+if [ -f $TMP ]; then
+	echo "complete... new file located at $2"
+	cat $TMP | sort | uniq > $2
+	rm "$TMP" 2>/dev/null
+else
+	echo "completed temp file is missing, cannot copy to destination"
+fi
